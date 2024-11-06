077104d7ba07db805af04f49532f21bebf270faf
gst <gustavo_souza12@estudante.sesisenai.org.br>
2024-11-06 15:05:36 -0300

    save

---
+++ questions.js
@@ -0,0 +1,110 @@
+export default [
+    {
+        question: "Quais Seus Pontos Fortes?",
+        answers: [
+            { option: "Pessoal", correct: true },
+            { option: "Futuro", correct: false },
+            { option: "Experiência", correct: false },
+            { option: "Empresa", correct: false },
+        ],
+    },
+    {
+        question: "Onde você se vê daqui cino anos? E em dez? ",
+        answers: [
+            {option:"Pessoal", correct: false},
+            {option:"Futuro", correct: true},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "Conte sobre uma vez que você cometeu um erro. ",
+        answers: [
+            {option:"Pessoal", correct: false},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: true},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "Qual foi sua maior falha? ",
+        answers: [
+            {option:"Pessoal", correct: false},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: true},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "O que te motiva? ",
+        answers: [
+            {option:"Pessoal", correct: true},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "Qual o nome do nosso CEO/Presidente? ",
+        answers: [
+            {option:"Pessoal", correct: false},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: true},
+        ],
+    },
+    {
+        question: "Você é um lider ou um seguidor? ",
+        answers: [
+            {option:"Pessoal", correct: true},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: false},
+        ]
+    },
+    {
+        question: "Qual a sua pretenção salarial? ",
+        answers: [
+            {option:"Pessoal", correct: false},
+            {option:"Futuro", correct: true},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "Quais são seus hobbies? ",
+        answers: [
+            {option:"Pessoal", correct: true},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "Como você lida com a pressão? ",
+        answers: [
+            {option:"Pessoal", correct: true},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "Me fale sobre a sua formação acadêmica. ",
+        answers: [
+            {option:"Pessoal", correct: false},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: true},
+            {option:"Empresa", correct: false},
+        ],
+    },
+    {
+        question: "Quais são os pontos fortes e fracos do seu chefe? ",
+        answers: [
+            {option:"Pessoal", correct: false},
+            {option:"Futuro", correct: false},
+            {option:"Experiencia", correct: false},
+            {option:"Empresa", correct: true},
+        ],
+    }
+]
---
+++ script2.js
@@ -0,0 +1,72 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+import questions from "./questions.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+    content.style.display = "flex";
+    contentFinish.style.display = "none";
+
+    currentIndex = 0;
+    questionsCorrect = 0;
+    loadQuestion();
+};
+
+function nextQuestion(e) {
+    const selectedButton = e.target;
+
+    if (selectedButton.getAttribute("data-correct") === "true") {
+        questionsCorrect++;
+        selectedButton.classList.add("correct-answer");
+    } else {
+        selectedButton.classList.add("incorrect-answer");
+    }
+
+    setTimeout(() => {
+        if (currentIndex < questions.length - 1) {
+            currentIndex++;
+            loadQuestion();
+        } else {
+            finish();
+        }
+    }, 1000); 
+}
+
+function finish() {
+    textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+    spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+    const item = questions[currentIndex];
+    answers.innerHTML = "";
+    question.innerHTML = item.question;
+
+    item.answers.forEach((answer) => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+      <button class="answer" data-correct="${answer.correct}">
+        ${answer.option}
+      </button>
+    `;
+
+        answers.appendChild(div);
+    });
+
+    document.querySelectorAll(".answer").forEach((item) => {
+        item.addEventListener("click", nextQuestion);
+    });
+}
+
+loadQuestion();
